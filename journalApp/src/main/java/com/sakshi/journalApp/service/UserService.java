0e10f8b1d11d52b07c207e8334fcef15fcb14c6d
git-user <you@example.com>
2026-06-23 18:50:25 +0530

    journal entry sppring security is added

---
+++ journalApp/src/main/java/com/sakshi/journalApp/service/UserService.java
@@ -0,0 +1,47 @@
+package com.sakshi.journalApp.service;
+
+import com.sakshi.journalApp.entity.Users;
+import com.sakshi.journalApp.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+@Slf4j
+public class UserService {
+    @Autowired
+    private UserRepository userRepository; //injection
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    
+
+    public void saveNewUser(Users user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles(Arrays.asList("USER"));
+        userRepository.save(user);
+    }
+
+    public void saveUser(Users user) {
+        userRepository.save(user);
+    }
+
+    public void saveAdmin(Users user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles(Arrays.asList("USER","ADMIN"));
+        userRepository.save(user);
+    }
+
+    public List<Users> getAll() {
+        return userRepository.findAll();
+    }
+
+    public Users findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+}
